7f1615511e446281bbcefa41e85f8eef5a2c69c0
ShamailovMax <max24june@gmail.com>
2022-08-04 00:40:11 +0300

    task 1 of 2.4 part

---
+++ learnjs-2/learnjs-2_4/task/index.js
@@ -0,0 +1,13 @@
+/* Задача взята отсюда: https://wm-school.ru/js/js_variables.php
+
+УСЛОВИЕ: 
+Объявите переменную с именем test и присвойте ему значение – 7. 
+Затем создайте другую переменную с именем test1. 
+Скопируйте значение пременной test в test1. 
+Выведите значение переменной test1. Выведите тип данных переменной test1.*/
+
+let test = 7
+let test1
+test1 = test
+console.log(test1)              // 7
+console.log(typeof test1)       // number
