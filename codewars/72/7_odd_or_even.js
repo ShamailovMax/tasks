70d4366ef6e0ab942c0f20e85d95de52286067d6
ShamailovMax <max24june@gmail.com>
2022-08-14 01:22:21 +0300

    meet #72 v3

--- codewars/72/7_odd_or_even.js
+++ codewars/72/7_odd_or_even.js
@@ -1,7 +1,5 @@
 function oddOrEven(array) {
   let summ = 0;
-  for (let i = 0; i < array.length; i++) {
-    summ += array[i];
-  }
+  for (let i = 0; i < array.length; i++) summ += array[i];
   return summ % 2 === 0 ? "even" : "odd";
 }
