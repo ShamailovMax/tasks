1a3837fc3f5ca4608f24eda303dd6560ee48613d
ShamailovMax <max24june@gmail.com>
2022-08-14 01:09:46 +0300

    meet #72 v2

--- codewars/72/8_lost_without_map.js
+++ codewars/72/8_lost_without_map.js
@@ -1,9 +1,9 @@
-// 1 способ
+// 1 способ - через map
 function withMap(x) {
   return x.map((item) => item * 2);
 }
 
-// 2 способ
+// 2 способ - без map
 function withoutMap(x) {
   for (let i = 0; i < x.length; i++) {
     x[i] *= 2;
