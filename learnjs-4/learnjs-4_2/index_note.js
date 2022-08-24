26097e754ead21cfc27159e8e0e624ee2a68d2eb
ShamailovMax <max24june@gmail.com>
2022-08-24 14:18:44 +0300

    meet #75 v5

--- learnjs-4/learnjs-4_2/index_note.js
+++ learnjs-4/learnjs-4_2/index_note.js
@@ -19,13 +19,13 @@ console.log(admin.name, user.name); // Tom Tom
 let a = {};
 let b = a;
 
-console.log(a == b);    // true
-console.log(a === b);   // true
+console.log(a == b); // true
+console.log(a === b); // true
 
 // теперь попробуеем вот так
-let c = {}
+let c = {};
 
-console.log(a == c)     // false
+console.log(a == c); // false
 
 // -----------------------------------
 // клонирование и объединение объектов
@@ -43,10 +43,10 @@ for (let key in user) {
   clone[key] = user[key];
 }
 
-clone.name = 'Mad'
+clone.name = "Mad";
 
-console.log(user.name);     // Chak
-console.log(clone.name)     // Mad
+console.log(user.name); // Chak
+console.log(clone.name); // Mad
 
 // Object.assign
 let user = { name: "Vasily" };
@@ -56,3 +56,28 @@ let user4 = { children: 7 };
 
 Object.assign(user, user2, user3, user4);
 console.log(user); // { name: 'Vasily', age: 40, hasIpoteka: true, children: 7 }
+
+// еще один пример
+let user = { userName: "James" };
+let clone = Object.assign({}, user);
+console.log(clone); // { userName: 'James' }
+
+// -----------------------------------------
+// Вложенное клонирование
+let user = {
+  name: "Max",
+  sizes: {
+    height: 176,
+    weight: 70,
+  },
+};
+
+console.log(user.sizes.weight); // 70
+
+let clone = Object.assign({}, user);
+console.log(user.sizes === clone.sizes); // true, так как один и тот же объект
+
+user.sizes.height++;
+
+console.log(user.sizes.height);
+console.log(clone.sizes.height);
