4cfa17b188f8b70fe31afb02396a04d3e1502011
ShamailovMax <max24june@gmail.com>
2022-08-24 13:44:25 +0300

    meet #75 v3

--- learnjs-4/learnjs-4_2/index_note.js
+++ learnjs-4/learnjs-4_2/index_note.js
@@ -8,3 +8,16 @@ let user = {
 };
 
 let admin = user; // копируется ссылка
+
+admin.name = "Tom";
+
+console.log(admin.name, user.name); // Tom Tom
+
+// так вышло, потому что изменения видны по ссылке из переменной user
+
+// сравнение по ссылке
+let a = {};
+let b = a;
+
+console.log(a == b);    // true
+console.log(a === b);   // true
