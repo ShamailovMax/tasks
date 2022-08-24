0e3f1a6e9a5b117681373e1ab54909381110ee94
ShamailovMax <max24june@gmail.com>
2022-08-24 14:01:38 +0300

    meet #75 v4

--- learnjs-4/learnjs-4_2/index_note.js
+++ learnjs-4/learnjs-4_2/index_note.js
@@ -21,3 +21,38 @@ let b = a;
 
 console.log(a == b);    // true
 console.log(a === b);   // true
+
+// теперь попробуеем вот так
+let c = {}
+
+console.log(a == c)     // false
+
+// -----------------------------------
+// клонирование и объединение объектов
+// Object.assign
+
+let user = {
+  name: "Chak",
+  age: 44,
+};
+
+let clone = {};
+
+// копируем все св-ва из user в clone
+for (let key in user) {
+  clone[key] = user[key];
+}
+
+clone.name = 'Mad'
+
+console.log(user.name);     // Chak
+console.log(clone.name)     // Mad
+
+// Object.assign
+let user = { name: "Vasily" };
+let user2 = { age: 40 };
+let user3 = { hasIpoteka: true };
+let user4 = { children: 7 };
+
+Object.assign(user, user2, user3, user4);
+console.log(user); // { name: 'Vasily', age: 40, hasIpoteka: true, children: 7 }
