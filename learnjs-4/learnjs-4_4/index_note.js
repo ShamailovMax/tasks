3c43541adcc98ea47d07b8e7065ee3439f25bb1b
ShamailovMax <max24june@gmail.com>
2022-08-25 12:14:45 +0300

    meet #75 v9

--- learnjs-4/learnjs-4_4/index_note.js
+++ learnjs-4/learnjs-4_4/index_note.js
@@ -8,4 +8,25 @@ let user = {
   },
 };
 
-user.greeting()
+user.greeting();
+
+// this не является фиксированным
+function sayHi() {
+  console.log(this.name);
+}
+
+
+let user1 = {name: 'Sergei'}
+let user2 = {name: 'Alexei'}
+
+user1.f = sayHi
+user2.f = sayHi
+
+user1.f()     // Sergei
+user2.f()     // Alexei
+
+user2['f']()  // Alexei
+
+// при вызове obj.f() значение this внутри f равно obj
+
+
