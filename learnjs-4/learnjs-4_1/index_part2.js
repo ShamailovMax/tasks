4cf92641f9dd368b3dcf4f24b542aa1a09c6df11
ShamailovMax <max24june@gmail.com>
2022-08-29 07:18:39 +0300

    долг по объектам - done

--- learnjs-4/learnjs-4_1/index_part2.js
+++ learnjs-4/learnjs-4_1/index_part2.js
@@ -8,8 +8,8 @@ const obj = {
 console.log(obj.property); // value
 console.log(obj['prop erty']) // 1
 
-// console.log(obj[property]) // Ошибка! Property is not defined (ReferenceError)
+console.log(obj[property]) // Ошибка! Property is not defined (ReferenceError)
 
 console.log(obj.береза) // undefined
-// console.log(null.property) // TypeError
-// console.log(undefined.property) // TypeError
+console.log(null.property) // TypeError
+console.log(undefined.property) // TypeError
