781d998b5a33b785a0eed8aa8c97a20df71c4daf
ShamailovMax <max24june@gmail.com>
2022-09-05 02:01:05 +0300

    not important changes

--- learnjs-4/global/order/index.js
+++ learnjs-4/global/order/index.js
@@ -2,12 +2,15 @@ function Order() {
   this.addItem = function (item, count) {
     this.item = prompt("Название позиции", item);
     this.count = prompt("Цена позиции", count);
+
+    // this.item = item;
+    // this.count = count;
   };
 
   this.removeItem = function (item, count) {};
 
   this.getCheck = function () {
-    return `Товар: ${this.item} Цена: ${this.count} руб.`;
+    return `${this.item} ... ${this.count} руб.`;
   };
 
   this.lockOrder = function () {};
