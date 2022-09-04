d1ed3b39ff592bef341d68d883a917adf7abf6f6
ShamailovMax <max24june@gmail.com>
2022-09-04 18:51:01 +0300

    task from discord in processing ...

---
+++ learnjs-4/global/order.js
@@ -0,0 +1,7 @@
+function Order() {
+  this.addItem = function (item, count) {};
+  this.removeItem = function (item, count) {};
+  this.getCheck = function () {};
+  this.lockOrder = function () {};
+  this.unlockOrder = function () {};
+}
