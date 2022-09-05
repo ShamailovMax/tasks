8b6b34248a00cac974acc3924f89a2a5603f37d0
ShamailovMax <max24june@gmail.com>
2022-09-05 04:39:20 +0300

    task from #задания is not solved yet

--- learnjs-4/global/order/index.js
+++ learnjs-4/global/order/index.js
@@ -1,15 +1,19 @@
 function Order() {
+  let orders = [];
   this.addItem = function (item, count) {
-    this.item = prompt("Название позиции", item);
-    this.count = prompt("Цена позиции", count);
+    // this.item = prompt("Название позиции", item);
+    // this.count = prompt("Цена позиции", count);
 
-    // this.item = item;
-    // this.count = count;
+    this.item = item;
+    this.count = count;
   };
 
   this.removeItem = function (item, count) {};
 
   this.getCheck = function () {
+    // console.log(orders);
+    orders = [{ item: this.item, count: this.count }];
+    console.log(orders);
     return `${this.item} ... ${this.count} руб.`;
   };
 
@@ -18,5 +22,7 @@ function Order() {
 }
 
 let order = new Order();
-order.addItem();
+order.addItem("Чай", 95);
+order.addItem("Ча", 95);
+order.addItem("Ч", 95);
 console.log(order.getCheck());
