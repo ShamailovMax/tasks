978e377d414d2a73d2e95d2e287a4035c1823b05
ShamailovMax <max24june@gmail.com>
2022-09-05 18:55:39 +0300

    example of ljs 4.8 [addings]

--- learnjs-4/learnjs-4_8/example.js
+++ learnjs-4/learnjs-4_8/example.js
@@ -7,9 +7,14 @@ function User(name, age) {
   // hint может быть только 'string', 'number', 'default'
   this[Symbol.toPrimitive] = (hint) => {
     if (hint === "string") return this.name;
-    if (hint === "number") return this.age;
+    return this.age
   };
 }
 
-let user = new User("Max", 21);
-console.log(String(user));
+let max = new User("Max", 21);
+let denis = new User("Denis", 24);
+
+console.log(String(max)); // Max
+console.log(Number(denis)); // 24
+console.log(denis - max); // 3
+console.log(max + denis); // 45
